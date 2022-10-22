54979da37699d16a6aac41639c8ee09b458e4e7f
ShanmuCode27 <90304223+ShanmuCode27@users.noreply.github.com>
2022-10-22 22:30:12 +0530

    tried js file for auth

---
+++ Javascripts/auth.js
@@ -0,0 +1,44 @@
+createAuth0Client({
+  domain: "dev-o7j10t6opbyd3gyu.us.auth0.com",
+  client_id: "ZpK8Ua26oPx0BNHDzNAJN8fTXAu8vtBj",
+  redirect_uri: window.location.origin,
+}).then(async (auth0) => {
+  // Assumes a button with id "login" in the DOM
+  const loginButton = document.getElementById("login");
+
+  loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth0.loginWithRedirect();
+  });
+
+  if (location.search.includes("state=") && 
+      (location.search.includes("code=") || 
+      location.search.includes("error="))) {
+    await auth0.handleRedirectCallback();
+    window.history.replaceState({}, document.title, "/");
+  }
+
+  // Assumes a button with id "logout" in the DOM
+  const logoutButton = document.getElementById("logout");
+
+  logoutButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    auth0.logout();
+  });
+
+  const isAuthenticated = await auth0.isAuthenticated();
+  const userProfile = await auth0.getUser();
+
+  // Assumes an element with id "profile" in the DOM
+  const profileElement = document.getElementById("profile");
+
+  if (isAuthenticated) {
+    profileElement.style.display = "block";
+    profileElement.innerHTML = `
+            <p>${userProfile.name}</p>
+            <img src="${userProfile.picture}" />
+          `;
+  } else {
+    profileElement.style.display = "none";
+  }
+});
